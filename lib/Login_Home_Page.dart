5d6e836afec812c3b9c442db66a8fe5953118fab
Vikas Uniyal <vikasuniyal762@gmail.com>
2023-07-14 19:41:53 +0530

    Commit

--- lib/Login_Home_Page.dart
+++ lib/Login_Home_Page.dart
@@ -7,11 +7,10 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-
   final _formKey = GlobalKey<FormState>();
   late TextEditingController _emailController;
   late TextEditingController _passwordController;
-  final bool _isEmailValid = false;
+  bool _isEmailValid = false; // Initialize to false
 
   @override
   void initState() {
@@ -49,98 +48,89 @@ class _LoginPageState extends State<LoginPage> {
                 borderRadius: BorderRadius.circular(50),
                 color: Colors.white,
               ),
-
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-
-
-                  ///EMAIL  CODE............
-
-                  SizedBox(
-                    width: 350,
-                      ///EMAIL  UI CODE............
-                child:TextFormField(
-                controller: _emailController,
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(
-                  hintText: "Please enter your email address",
-                  prefixIcon: Icon(Icons.mail, color: Color(0xff03168a)),
-                ),
-                  ///EMAIL  VALIDATION  CODE............
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your email address';
-                  }
-                  if (!_isEmailValidFormat(value)) {
-                    return 'Please enter a valid email address';
-                  }
-                  return null;
-                },
-              )
-                  ),
-
-                  ///PASSWORD CODE............
-                  SizedBox(
-                    ///PASSWORD UI CODE............
-                    width: 350,
-                    child: TextFormField(
-                      controller: _passwordController,
-                      keyboardType: TextInputType.text,
-                      obscureText: true,
-                     // enabled: _isEmailValid,
-                      decoration: InputDecoration(
-                        hintText: "Please enter your password",
-                        prefixIcon: const Icon(Icons.lock, color: Color(0xff03168a)),
-                        enabledBorder: _isEmailValid
-                            ? null
-                            : const OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 350,
+                      child: TextFormField(
+                        controller: _emailController,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: const InputDecoration(
+                          hintText: "Please enter your email address",
+                          prefixIcon: Icon(Icons.mail, color: Color(0xff03168a)),
                         ),
-                      ),
-                      ///PASSWORD validation CODE............
-                      validator: (value) {
-                        if (_isEmailValid) {
+                        validator: (value) {
                           if (value == null || value.isEmpty) {
-                            return 'Please enter your password';
+                            return 'Please enter your email address';
                           }
-                          // Add additional password validation if needed
-                        }
-                        return null;
-                      },
-                    ),
-                  ),
-
-///LOGIN BUTTON CODE............
-                  Padding(
-                    padding: const EdgeInsets.only(top: 40.0),
-                    child: SizedBox(
-                      width: 200,
-                      child: RawMaterialButton(
-                        fillColor: const Color(0xFF0069FE),
-                        elevation: 0.0,
-                        padding: const EdgeInsets.symmetric(vertical: 12.0),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20.0),
-                        ),
-                        onPressed: () async {
-                          if (_formKey.currentState!.validate()) {
-                            // Form is valid, perform login operation
-                            // You can access the email and password using _emailController.text and _passwordController.text respectively
-                           // await performLogin();
+                          if (!_isEmailValidFormat(value)) {
+                            return 'Please enter a valid email address';
                           }
+                          _isEmailValid = true; // Set to true if email is valid
+                          return null;
                         },
-                        child: const Text(
-                          "Login",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18.0,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 350,
+                      child: TextFormField(
+                        controller: _passwordController,
+                        keyboardType: TextInputType.text,
+                        obscureText: true,
+                        enabled: _isEmailValid, // Enable only if email is valid
+                        decoration: InputDecoration(
+                          hintText: "Please enter your password",
+                          prefixIcon: const Icon(Icons.lock, color: Color(0xff03168a)),
+                          enabledBorder: _isEmailValid
+                              ? null
+                              : const OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (_isEmailValid) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your password';
+                            }
+                            // Add additional password validation if needed
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 40.0),
+                      child: SizedBox(
+                        width: 200,
+                        child: RawMaterialButton(
+                          fillColor: const Color(0xFF0069FE),
+                          elevation: 0.0,
+                          padding: const EdgeInsets.symmetric(vertical: 12.0),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                          onPressed: () async {
+                            if (_formKey.currentState!.validate()) {
+                              // Form is valid, perform login operation
+                              // You can access the email and password using _emailController.text and _passwordController.text respectively
+                              // await performLogin();
+                            }
+                          },
+                          child: const Text(
+                            "Login",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18.0,
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
@@ -148,11 +138,10 @@ class _LoginPageState extends State<LoginPage> {
       ),
     );
   }
-}
-
-bool _isEmailValidFormat(String value) {
-  final RegExp emailRegex = RegExp(
-    r'^[\w-]+(\.[\w-]+)*@[a-zA-Z\d-]+(\.[a-zA-Z\d-]+)*\.[a-zA-Z\d-]{2,}$',
-  );
-  return emailRegex.hasMatch(value);
+  bool _isEmailValidFormat(String value) {
+    final RegExp emailRegex = RegExp(
+      r'^[\w-]+(\.[\w-]+)*@[a-zA-Z\d-]+(\.[a-zA-Z\d-]+)*\.[a-zA-Z\d-]{2,}$',
+    );
+    return emailRegex.hasMatch(value);
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.green
       ),
      // home: SecondPageState(),
-       // home:const LoginPage(),
+      home:const LoginPage(),
     );
   }
 }
